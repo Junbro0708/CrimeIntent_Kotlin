9248dd23ef34fc637eced8021a78c6cdabe5077d
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-08-27 20:59:48 +0900

    I studied kotlin about fragment in Android

--- app/src/main/java/com/example/crimeintent/MainActivity.kt
+++ app/src/main/java/com/example/crimeintent/MainActivity.kt
@@ -13,11 +13,13 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val fm: FragmentManager = supportFragmentManager
         var fragment: Fragment? = fm.findFragmentById(R.id.frag_l)
         if (fragment == null) {
             fragment = CrimeFragment()
             fm.beginTransaction().add(R.id.frag_l, fragment).commit()
+
         }
     }
 }
